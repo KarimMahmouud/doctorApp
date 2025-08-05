71d56ad35f3262c51c3fc03c61bcb09a75ffe262
KarimMahmouud <karimgad461@gmail.com>
2025-08-05 14:51:21 +0300

    Launching project

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../features/login/ui/screens/login_screen.dart';
+import '../../features/onBoarding/ui/screens/onBoarding_screen.dart';
+import 'routes.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnBoardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/doc_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced/core/routing/app_router.dart';
+import 'package:flutter_advanced/core/routing/routes.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/theming/colors.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
+// handel MaterialApp and Navigation_extension
